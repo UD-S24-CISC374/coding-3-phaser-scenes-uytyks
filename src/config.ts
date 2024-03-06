469cdfc51453f479fd13a0f7e6e40897c0265ff7
uytyks <uytyks@udel.edu>
2024-03-06 17:10:06 -0500

    Modern changes

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 import MainScene from "./scenes/mainScene";
 import PreloadScene from "./scenes/preloadScene";
+import Scene2 from "./scenes/sceneTwo";
 
 const DEFAULT_WIDTH = 1280;
 const DEFAULT_HEIGHT = 720;
@@ -17,7 +18,7 @@ export const CONFIG = {
         width: DEFAULT_WIDTH,
         height: DEFAULT_HEIGHT,
     },
-    scene: [PreloadScene, MainScene],
+    scene: [PreloadScene, MainScene, Scene2],
     physics: {
         default: "arcade",
         arcade: {
